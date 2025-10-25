7189919a8ea0d17fb2bee81f2b21ff46d137d77a
snigdha <snigdhasundriyal23@gmail.com>
2025-10-25 17:37:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
+screen.title("turtle cross game. 😊 ")
+
+scoreboard=Scoreboard()
+player=Player()
+car_manager=CarManager()
+screen.listen()
+screen.onkey(player.move,"Up")
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    car_manager.create_cars()
+    car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player)<20:
+            game_is_on=False
+            scoreboard.game_over()
+
+    if player.is_at_finish_line():
+        player.goto_start()
+        car_manager.level_up()
+        scoreboard.increase_level()
+
+screen.exitonclick()
